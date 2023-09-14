81998c82abbbd0da0ac0e288cda9ae190c6a5eb4
behshad <82580731+biigii@users.noreply.github.com>
2023-09-14 21:03:09 +1200

    javascript

---
+++ main.js
@@ -0,0 +1,79 @@
+//  ( ) { } [] 
+//  grandparent().parent({}).child.grandchild
+
+
+// here hello world is a string, ' ' . or " "
+// alert('hello world')
+
+// string which looks like "hello world"
+// numbers 100 200 1 0
+// boolean true or false
+// console.log('hello world')
+// console.log(1000)
+// console.log(3000+2000)
+// console.log(true)
+
+// variables 
+var num = 500;
+num = 5000;
+// console.log(num);
+console.log(num + num)
+
+// var name = "hello"
+// var name2 = " world"
+
+// console.log(name+name2)
+
+// console.log(1000 + '1000');
+// var myname = 'behshad'
+// camel casing
+var myName = "some name"
+var myAwesomeCode = 1000;
+// === this is comparison
+
+// array or lists
+var fruits = ['apple', 'orange', 'kiwi']
+var random = [1, 'hello world', 1000, true, 0]
+
+//  instead of var we now use const and let
+
+// var x
+// x = 100
+// console.log(x)
+// x = 'hello'
+// console.log(x)
+// var x = true
+// const and let
+// const x = 'hello'
+
+// let y
+// y = 20
+// y = 'hello world'
+
+// document.body.style.backgroundColor = "#00f"
+// document.body.style.color = "#fff"
+
+// document.getElementById("heading").style.color = "#f11"
+// document.getElementById("heading").style.fontSize = '5em'
+// document.getElementById("heading").style.border = "10px solid #0f0"
+
+// const h1 = document.getElementById("heading");
+
+// h1.style.color = "red"
+// h1.style.fontSize = "5em"
+
+// const text = document.getElementById("text");
+// text.style.border = "3px solid"
+// document.getElementsByClassName('content')
+// const h1 = document.getElementsByTagName('h1')[0]
+// h1.style.color = "red"
+
+{/* <h1 id="heading" class="heading">javascript</h1> */}
+// document.querySelector('h1')
+// document.querySelector("#heading")
+// document.querySelector(".heading")
+// document.querySelector("h1.heading#heading")
+
+
+
+
